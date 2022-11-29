733842181e97f353c70f51b7b81c4585895dbfff
amalia0007 <amaliastefanescu0@gmail.com>
2022-11-29 19:55:12 +0200

    refactor rename

--- src/main/java/ro/unibuc/fmi/expensetracker/singleton/LogApplicationRequests.java
+++ src/main/java/ro/unibuc/fmi/expensetracker/singleton/LogApplicationRequests.java
@@ -6,15 +6,15 @@ import java.io.PrintWriter;
 import java.time.Instant;
 
 public class LogApplicationRequests {
-    private PrintWriter writer;
+    private PrintWriter printWriter;
 
     private static LogApplicationRequests logApplicationRequests = null;
 
     private LogApplicationRequests() {
         try {
-            String logFile = "logDateRequests.txt";
-            FileWriter fw = new FileWriter(logFile);
-            writer = new PrintWriter(fw, true);
+            String fileName = "logDateRequests.txt";
+            FileWriter fileWriter = new FileWriter(fileName);
+            printWriter = new PrintWriter(fileWriter, true);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,12 +28,12 @@ public class LogApplicationRequests {
 
     public void logTripCreation() {
         System.out.println("Logging successful");
-        writer.println("A request to create a Trip was initiated on date: " + Instant.now());
+        printWriter.println("A request to create a Trip was initiated on date: " + Instant.now());
     }
 
     public void logUserCreation() {
         System.out.println("Logging successful");
-        writer.println("A request to create a User was initiated on date: " + Instant.now());
+        printWriter.println("A request to create a User was initiated on date: " + Instant.now());
     }
 
 }
